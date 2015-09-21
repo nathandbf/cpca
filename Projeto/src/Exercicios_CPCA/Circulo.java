5eb89a2f62ea154b8bdfd734cd5d388d3a2300c8
pcqs <pcqs@192.168.100.251>
2015-09-21 16:06:44 -0300

    ex1

--- Projeto/src/Exercicios_CPCA/Circulo.java
+++ Projeto/src/Exercicios_CPCA/Circulo.java
@@ -2,4 +2,66 @@ package Exercicios_CPCA;
 
 public class Circulo {
 
+	private int raio;
+	private int x;
+	private int y;
+	
+	public Circulo(){
+		raio =1;
+		x=1;
+		y=1;
+		
+	}
+	public Circulo(int raio, int x, int y) {
+		this.raio = raio;
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getRaio() {
+		return raio;
+	}
+	
+	public void setRaio(int raio) {
+		this.raio = raio;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int aumentarCirculo(){
+		raio++;
+		return raio;
+	}
+	
+	public int diminuirCirculo(){
+		raio--;
+		return raio;
+	}
+	public void moverCirculo(int x,int y ){
+		this.x=x;
+		this.y=y;
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "O Circulo possue raio de " + raio + ",tendo seu x em " + x + "e seu y em " + y + ".";
+	}
+	
+	
+	
 }
